c419a551e0f329d5595840b655346d6beab2878f
InvictusTTV <44098372+InvictusTTV@users.noreply.github.com>
2020-05-07 16:48:13 +0530

    Updates

---
+++ precache-manifest.a7874dab57c4c9927a326609b2b8b163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00ebf1730069af6e2abbddafa28b33f7",
+    "url": "/monsters-react/index.html"
+  },
+  {
+    "revision": "35a47ea3a6c63101b384",
+    "url": "/monsters-react/static/css/main.b2fcb6e5.chunk.css"
+  },
+  {
+    "revision": "d2dc50b11a291cc06e27",
+    "url": "/monsters-react/static/js/2.b09687aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react/static/js/2.b09687aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35a47ea3a6c63101b384",
+    "url": "/monsters-react/static/js/main.1ad0c044.chunk.js"
+  },
+  {
+    "revision": "43b2414ca5f197dc12f2",
+    "url": "/monsters-react/static/js/runtime-main.cfd33fbe.js"
+  }
+]);
